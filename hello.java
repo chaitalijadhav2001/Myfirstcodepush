e30870c6aec703f5bb6825e083341a11b364570f
chaitalijadhav2001 <86162445+chaitalijadhav2001@users.noreply.github.com>
2025-08-19 18:31:43 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
  class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! Chaitali Have 3 Years of experience and she is Devops Engineer .");
     }
 }
